34c119ff5d92254606a63c0937a8acc64526811d
mahrous-amer <eng.mahrous.amer@gmail.com>
2020-03-24 22:31:24 +0800

    updating_Django_project

--- spotify_grinder/spotify_grinder/settings.py
+++ spotify_grinder/spotify_grinder/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'grinder.apps.GrinderConfig',
+    'apps.grinder.apps.GrinderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
