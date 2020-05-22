751bc359b5ee20cb2ecfa55c06774f10e81f9e2d
mahrous-amer <eng.mahrous.amer@gmail.com>
2020-05-22 16:52:52 +0800

    adding simple front-end html landing page and adjusting api_wrapper.py which will need to create a file in the directory /app/grinder_api/config.cfg including a title [SPOTIFY] also the file should contian CLIENT_ID=***** and CLIENT_SECRET=***** provided by spotify while you are there add a callback url in your app settings

--- app/grinder/views.py
+++ app/grinder/views.py
@@ -2,24 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .api_wrapper import get_track_list
 
 def index(request):
-    #return HttpResponse("Hello, world. You're at the spotify-grinder's index.")
-    '''
-    if no query_filter is provided in the request we query with the default track filter
-    validations of the query are handled by the api_wrapper instead of the view.
-    '''
-    search_query = request.GET.get('q')
-    query_filter = request.GET.get('filter')
-    if not query_filter:
-        query_filter = 'track'
-
-    count, items = get_track_list(search_query, query_filter)
-    context = {
-        'count' : count,
-        'items': items,
-        'q': search_query,
-        'filter' : query_filter
-    }
-    return render(request, 'index2.html', context)
+    return render(request, 'base.html')
