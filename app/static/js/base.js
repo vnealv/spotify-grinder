0f262dbc34bbb2d3bdd55bbf54e227a6f1766f47
mahrous-amer <eng.mahrous.amer@gmail.com>
2020-05-23 12:04:13 +0800

    further enhancing the look of the initial landing page

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2021 15:37:25").getTime();
+var countDownDate = new Date("April 20, 2021 04:20:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
